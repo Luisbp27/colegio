f02deddc974c91c08f6450da6193b80676002034
Luis Barca <luisbarcapons@gmail.com>
2021-11-23 13:10:08 +0100

    Extra

--- src/Main/GestionColegios.java
+++ src/Main/GestionColegios.java
@@ -1,5 +1,10 @@
 package Main;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author luisb
@@ -10,7 +15,11 @@ public class GestionColegios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(GestionColegios.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
