92a849667d03e8994b3483405a52c895bb75acec
Luisbp27 <luisbarcapons@gmail.com>
2021-11-28 13:07:55 +0100

    Temp

--- src/Asignatura/Asignatura.java
+++ src/Asignatura/Asignatura.java
@@ -1,7 +1,9 @@
 package Asignatura;
 
 import Curso.Curso;
+import Estudiante.Estudiante;
 import Interfaz.InterfazDatos;
+import Lista_Ref.Lista_Ref_Estudiantes;
 
 /**
  *
@@ -10,13 +12,37 @@ import Interfaz.InterfazDatos;
 public abstract class Asignatura implements InterfazDatos {
     
     protected String nombre;
-    protected int codigo;
-    
-    private Curso curso;
+    protected int codigo; 
+    protected String stringCurso;
+    protected Lista_Ref_Estudiantes lre;
     
     public Asignatura(String nombre, int codigo, Curso curso) {
         this.nombre = nombre;
         this.codigo = codigo;
-        this.curso = curso;
+        
+        this.lre = new Lista_Ref_Estudiantes();
     }
+    
+    @Override
+    public abstract String toString();
+    
+    public abstract String getNombre();
+    
+    public abstract int getCodigo();
+
+    public abstract void setNombre(String name);
+    
+    public abstract void setCodigo(int cod);
+
+    public abstract void setCurso(String x);
+
+    public abstract String getCurso();
+    
+    public abstract Estudiante getRefEstudiante(int i);
+
+    public abstract int getSizeRef();
+
+    public abstract void add(Estudiante e);
+
+    public abstract void remove(Estudiante e);
 }
--- src/Asignatura/Obligatoria.java
+++ src/Asignatura/Obligatoria.java
@@ -6,6 +6,7 @@
 package Asignatura;
 
 import Curso.Curso;
+import Estudiante.Estudiante;
 
 /**
  *
@@ -13,13 +14,78 @@ import Curso.Curso;
  */
 public class Obligatoria extends Asignatura {
     
-    private final int creditos = 6;
+    private static final int CREDITOS = 6;
 
     public Obligatoria(String nombre, int codigo, Curso curso) {
         super(nombre, codigo, curso);
     }
 
-    public int compareTo(Asignatura t) {
+    public int compareTo(Asignatura a) {
+        int resultado = this.nombre.compareTo(a.nombre);
+        
+        if (resultado < 0) {
+            return -1;
+        } else {
+            if (resultado == 0) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " CÓDIGO: " + codigo + " Nº Créditos: " + CREDITOS;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public int getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public void setCurso(String x) {
+        this.stringCurso = x;
+    }
+
+    @Override
+    public String getCurso() {
+        return stringCurso;
+    }
+
+    @Override
+    public Estudiante getRefEstudiante(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getSizeRef() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(Estudiante e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Estudiante e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
